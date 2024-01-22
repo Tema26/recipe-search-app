d6821391a1c772aadb998d5b41eac0c993c9a544
a.moskvitin <a.moskvitin@flatsharing.biz>
2024-01-23 00:00:37 +0300

    installed dependencies of vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: '',
+    plugins: [react(), viteTsconfigPaths()],
+    server: {
+        open: true,
+        port: 3000,
+    },
 })
